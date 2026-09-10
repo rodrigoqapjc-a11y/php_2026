767a9dc176d43508a9d3c45f7686bbc1aa169ba1
rodrigoqapjc-a11y <rodrigoqapjc@uni9.edu.br>
2026-09-10 00:41:56 +0000

    topico3

---
+++ src/topico03.php
@@ -0,0 +1,20 @@
+<?php
+$cep= "01012123";
+echo "cep = $cep";
+$valor=5124.784;
+echo"<br>valor = $valor";
+echo "<br>R$ ".number_format($valor,2,",",".");
+echo "<br>R$ ".number_format($valor,0,",",".");
+$nome= "Bete";
+echo "<br>nome = $nome";
+echo '<br>nome = $nome';
+$cor="green";
+echo "<p style='color:$cor'>$nome</p>";
+$inteiro=(int)$valor;
+echo "<br>valor = $inteiro";
+$decimal= $valor-$inteiro;
+echo "<br>valor = ".$decimal;
+printf("<br>valor = %.3f",$decimal);
+define("PI",3.1415);
+echo "<br>PI = " .PI;
+?>
